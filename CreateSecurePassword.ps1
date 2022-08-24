4a5507bdd573de19de5ff0482cdabcaecfae3d4a
coldborey <59556075+coldborey@users.noreply.github.com>
2022-08-24 13:22:22 +0300

    Update CreateSecurePassword.ps1

--- CreateSecurePassword.ps1
+++ CreateSecurePassword.ps1
@@ -1,2 +1,3 @@
+#This script encrypt password with created AES key
 $mypasswd = ConvertTo-SecureString "password" -AsPlainText -Force
 $mypasswd| ConvertFrom-SecureString -Key (get-content *path_to_keyfile*)| Set-Content *path_to_outputfile*
